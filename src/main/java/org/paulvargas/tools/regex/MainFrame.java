124b64297c8ddc2089f4c953b1558fadd1c0819f
PAUL VARGAS BAUTISTA <paul-vargas@users.noreply.github.com>
2016-11-11 18:03:47 -0600

    Added JTable for split.

--- src/main/java/org/paulvargas/tools/regex/MainFrame.java
+++ src/main/java/org/paulvargas/tools/regex/MainFrame.java
@@ -9,6 +9,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.regex.Pattern;
@@ -25,7 +27,10 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
+import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -34,17 +39,32 @@ import javax.swing.table.DefaultTableModel;
  */
 public class MainFrame extends javax.swing.JFrame {
 
-  private final DefaultTableModel groupModel;
-  private final DefaultTableModel splitModel;
+  private DefaultTableModel groupModel;
+  private DefaultTableModel splitModel;
 	
 	/**
 	 * Creates new form MainFrame
 	 */
 	public MainFrame() {
-		this.splitModel = new DefaultTableModel();
-		this.groupModel = new DefaultTableModel();
+		initTableModels();
 		initComponents();
 		initFlags();
+		regexTextField.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				System.out.println("insertUpdate: " + e);
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				System.out.println("removeUpdate: " + e);
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				System.out.println("changedUpdate: " + e);
+			}
+		});
 	}
 
 	/**
@@ -70,6 +90,8 @@ public class MainFrame extends javax.swing.JFrame {
         groupsScrollPane = new JScrollPane();
         groupsTable = new JTable();
         splitPanel = new JPanel();
+        splitScrollPane = new JScrollPane();
+        splitTable = new JTable();
         replacePanel = new JPanel();
         snippetPanel = new JPanel();
 
@@ -85,7 +107,7 @@ public class MainFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(regexLabel)
                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(regexTextField, GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+                .addComponent(regexTextField, GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)
                 .addContainerGap())
         );
         regexPanelLayout.setVerticalGroup(regexPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
@@ -115,22 +137,43 @@ public class MainFrame extends javax.swing.JFrame {
 
         splitPane.setLeftComponent(inputScrollPane);
 
-        groupsPanel.setLayout(new GridLayout());
-
         groupsTable.setModel(groupModel);
+        groupsTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         groupsScrollPane.setViewportView(groupsTable);
 
-        groupsPanel.add(groupsScrollPane);
+        GroupLayout groupsPanelLayout = new GroupLayout(groupsPanel);
+        groupsPanel.setLayout(groupsPanelLayout);
+        groupsPanelLayout.setHorizontalGroup(groupsPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(groupsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(groupsScrollPane, GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        groupsPanelLayout.setVerticalGroup(groupsPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(groupsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(groupsScrollPane, GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         tabbedPane.addTab("Matched Subsequences", groupsPanel);
+
+        splitTable.setModel(splitModel);
+        splitScrollPane.setViewportView(splitTable);
 
         GroupLayout splitPanelLayout = new GroupLayout(splitPanel);
         splitPanel.setLayout(splitPanelLayout);
         splitPanelLayout.setHorizontalGroup(splitPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 560, Short.MAX_VALUE)
+            .addGroup(splitPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(splitScrollPane, GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
+                .addContainerGap())
         );
         splitPanelLayout.setVerticalGroup(splitPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 109, Short.MAX_VALUE)
+            .addGroup(splitPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(splitScrollPane, GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         tabbedPane.addTab("Split", splitPanel);
@@ -138,10 +181,10 @@ public class MainFrame extends javax.swing.JFrame {
         GroupLayout replacePanelLayout = new GroupLayout(replacePanel);
         replacePanel.setLayout(replacePanelLayout);
         replacePanelLayout.setHorizontalGroup(replacePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 560, Short.MAX_VALUE)
+            .addGap(0, 492, Short.MAX_VALUE)
         );
         replacePanelLayout.setVerticalGroup(replacePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 109, Short.MAX_VALUE)
+            .addGap(0, 283, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Replace", replacePanel);
@@ -149,10 +192,10 @@ public class MainFrame extends javax.swing.JFrame {
         GroupLayout snippetPanelLayout = new GroupLayout(snippetPanel);
         snippetPanel.setLayout(snippetPanelLayout);
         snippetPanelLayout.setHorizontalGroup(snippetPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 560, Short.MAX_VALUE)
+            .addGap(0, 492, Short.MAX_VALUE)
         );
         snippetPanelLayout.setVerticalGroup(snippetPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 109, Short.MAX_VALUE)
+            .addGap(0, 283, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Snippet", snippetPanel);
@@ -163,10 +206,21 @@ public class MainFrame extends javax.swing.JFrame {
 
         getContentPane().add(mainPanel, BorderLayout.CENTER);
 
-        setSize(new Dimension(581, 481));
+        setSize(new Dimension(513, 542));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+	private void initTableModels() {
+		groupModel = new DefaultTableModel();
+		groupModel.addColumn("n");
+		groupModel.addColumn("start");
+		groupModel.addColumn("end");
+		groupModel.addColumn("group");
+		splitModel = new DefaultTableModel();
+		splitModel.addColumn("index");
+		splitModel.addColumn("value");
+	}
+	
 	private void initFlags() {
 		flagsPanel.setLayout(new WrapLayout(FlowLayout.LEFT));
 		for (Field field : Pattern.class.getDeclaredFields()) {
@@ -189,7 +243,7 @@ public class MainFrame extends javax.swing.JFrame {
 		 */
 		try {
 			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-				if ("Windows".equals(info.getName())) {
+				if ("Nimbus".equals(info.getName())) {
 					javax.swing.UIManager.setLookAndFeel(info.getClassName());
 					break;
 				}
@@ -229,6 +283,8 @@ public class MainFrame extends javax.swing.JFrame {
     private JPanel snippetPanel;
     private JSplitPane splitPane;
     private JPanel splitPanel;
+    private JScrollPane splitScrollPane;
+    private JTable splitTable;
     private JTabbedPane tabbedPane;
     // End of variables declaration//GEN-END:variables
 
